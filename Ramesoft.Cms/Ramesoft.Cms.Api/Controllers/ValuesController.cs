480128cc325c7095504ebf3f5e0cff59c791d3d7
hajirazin <hajirazin@hotmail.com>
2014-03-18 23:22:16 +0530

    Added web api project

---
+++ Ramesoft.Cms/Ramesoft.Cms.Api/Controllers/ValuesController.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValuesController.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The values controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Ramesoft.Cms.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    /// <summary>
+    /// The values controller.
+    /// </summary>
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        public void Delete(int id)
+        {
+        }
+
+        /// <summary>
+        /// The get.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        /// <summary>
+        /// The get.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        /// <summary>
+        /// The post.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        /// <summary>
+        /// The put.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        #endregion
+
+        // DELETE api/values/5
+    }
+}
---
+++ Ramesoft.Cms/Ramesoft.Cms.Api/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Global.asax.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The web api application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Ramesoft.Cms.Api
+{
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    /// <summary>
+    /// The web API application.
+    /// </summary>
+    public class WebApiApplication : HttpApplication
+    {
+        #region Methods
+
+        /// <summary>
+        /// The application_ start.
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        #endregion
+    }
+}
